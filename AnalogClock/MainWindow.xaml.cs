172e47e99e9e90f89cae0d8582e05e080411bfdf
Minelrem <millenium.minelrem@gmail.com>
2017-10-23 12:32:00 +0300

    Added Tick Tack variation

--- AnalogClock/MainWindow.xaml.cs
+++ AnalogClock/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Media;
 using System.Threading;
 using System.Windows;
 using System.Windows.Media;
@@ -12,192 +13,74 @@ namespace AnalogClock
     /// </summary>
     public partial class MainWindow : Window
     {
+         
+        Tuple<int, int> secs = Tuple.Create(0, 6);
 
-        #region init of degree(Type1) and current time(Type 2)  
-        Tuple<int,int> hours = Tuple.Create(0,0);
-        Tuple<int, int> mins = Tuple.Create(0, 0);
-        Tuple<int, int> secs = Tuple.Create(1, 1);
-        #endregion
-
-        DoubleAnimation secAnim;
-        DoubleAnimation minAnim;
-        DoubleAnimation hourAnim;
-
-
+        static string path = Environment.CurrentDirectory + @"\Sound\clockTick.wav";
+        SoundPlayer player = new SoundPlayer(path);
+         
+        DoubleAnimation secAnim; 
+         
         TimerCallback timCall;
 
         public MainWindow()
         {
             InitializeComponent();
-            timCall = new TimerCallback(refreshAnimation);
+            timCall = new TimerCallback(refreshAnimation); 
         }
 
         private void AnimBuilder()
         {
-            #region creating animations
             secAnim = new DoubleAnimation
             {
                 Name = "secAnim",
                 From = secs.Item1,
-                To = 360,
-                Duration = TimeSpan.FromSeconds(60 - secs.Item2)
+                To = secs.Item2,
+                Duration = TimeSpan.FromMilliseconds(875)
 
             };
 
             secAnim.Completed += SecAnim_Completed;
-
-            minAnim = new DoubleAnimation
-            {
-                Name = "minAnim",
-                From = mins.Item1,
-                To = 360,
-                Duration = TimeSpan.FromMinutes(60 - mins.Item2)
-
-            };
-
-            minAnim.Completed += MinAnim_Completed;
-
-            hourAnim = new DoubleAnimation
-            {
-                Name = "hourAnim",
-                From = hours.Item1,
-                To = 360,
-                Duration = TimeSpan.FromHours(12 - hours.Item2)
-
-            };
-
-            hourAnim.Completed += HourAnim_Completed;
-
-            #endregion
-
-            #region animation begin
+             
             secTrans.BeginAnimation(RotateTransform.AngleProperty, secAnim);
-            minTrans.BeginAnimation(RotateTransform.AngleProperty, minAnim);
-            hourTrans.BeginAnimation(RotateTransform.AngleProperty, hourAnim);
 
-            #endregion
-
-
+            player.Play();
         }
 
-        #region events subscription
-        private void HourAnim_Completed(object sender, EventArgs e)
-        {
-            object hourName = hourAnim.Name;
-
-            Timer hourTimer = new Timer(timCall, hourName, 1, 0);
- 
-
-        }
-
-        private void MinAnim_Completed(object sender, EventArgs e)
-        {
-
-            object minName = minAnim.Name;
-            Timer minTimer = new Timer(timCall, minName, 1, 0);
-
-        }
-
+       
         private void SecAnim_Completed(object sender, EventArgs e)
         {
 
-            object secName = secAnim.Name;
+            secs = Tuple.Create(secs.Item1 +6, secs.Item2+6);
 
-            Timer timer = new Timer(timCall, secName, 1, 0);
+            object obj= new object();
+            Timer timer = new Timer(timCall, obj, 150, 0);
 
         }
-        #endregion
+        
 
         void refreshAnimation(object obj)
         { 
-            switch ((string)obj) { 
-                case "minAnim":
-                    {
-
-                        mins = Tuple.Create(0, 1);
-
-                        Dispatcher.Invoke(() =>
-                        {
-                            minAnim.From = mins.Item1;
-                            minAnim.Duration = TimeSpan.FromMinutes(60);
-                            minAnim.RepeatBehavior = RepeatBehavior.Forever;
-                            minTrans.BeginAnimation(RotateTransform.AngleProperty, minAnim);
-                        });
-
-                        break;
-                    }
-                case "secAnim":
-                    {
-                       
-                        secs = Tuple.Create(0, 1);
+            if(secs.Item1 >= 360)
+                secs = Tuple.Create(0, 6);
                          
-                        Dispatcher.Invoke(() =>
-                        {
-                            secAnim.From = secs.Item1;
-                            secAnim.Duration = TimeSpan.FromSeconds(60);
-                            secAnim.RepeatBehavior = RepeatBehavior.Forever;
-                            secTrans.BeginAnimation(RotateTransform.AngleProperty, secAnim);
-                        });
-                        
-                        break;
-                    }
-                case "hourAnim":
-                    {
-                        hours = Tuple.Create(0, 1);
+            Dispatcher.Invoke(() =>
+            {
+                secAnim.From = secs.Item1;
+                secAnim.To = secs.Item2;
+                secAnim.Duration = TimeSpan.FromMilliseconds(875);
+                secTrans.BeginAnimation(RotateTransform.AngleProperty, secAnim);
+                player.Play();
 
-                        Dispatcher.Invoke(() =>
-                        {
-                            hourAnim.From = hours.Item1;
-                            hourAnim.Duration = TimeSpan.FromHours(60);
-                            hourAnim.RepeatBehavior = RepeatBehavior.Forever;
-                            hourTrans.BeginAnimation(RotateTransform.AngleProperty, hourAnim);
-                        });
-
-                        break;
-                    }
-                default: break;
-            }
-
+            }); 
         }
 
         private void Calc()
         {
             foreach (Rectangle tmp in Clock.Children)
             {
-                if (tmp == HourArrow)
-                {
-                    switch (DateTime.Now.Hour)
-                    {
-                        case 0: hours = Tuple.Create(0,DateTime.Now.Hour); break;
-                        case 1: hours = Tuple.Create(30, DateTime.Now.Hour); break;
-                        case 2: hours = Tuple.Create(60, DateTime.Now.Hour); break;
-                        case 3: hours = Tuple.Create(90, DateTime.Now.Hour); break;
-                        case 4: hours = Tuple.Create(120, DateTime.Now.Hour); break;
-                        case 5: hours = Tuple.Create(150, DateTime.Now.Hour); break;
-                        case 6: hours = Tuple.Create(180, DateTime.Now.Hour); break;
-                        case 7: hours = Tuple.Create(210, DateTime.Now.Hour); break;
-                        case 8: hours = Tuple.Create(240, DateTime.Now.Hour); break;
-                        case 9: hours = Tuple.Create(270, DateTime.Now.Hour); break;
-                        case 10: hours = Tuple.Create(300, DateTime.Now.Hour); break;
-                        case 11: hours = Tuple.Create(330, DateTime.Now.Hour); break;
-                        case 12: hours = Tuple.Create(360, DateTime.Now.Hour); break;
-                        default: hours = Tuple.Create(0, DateTime.Now.Hour); break;
-                    }
-                }
-                else if (tmp == MinuteArrow)
-                {
-                    int buff = DateTime.Now.Minute;
-                    int counter = 0;
-
-                    while (counter != buff)
-                    {
-                        Tuple<int, int> tm = Tuple.Create(((int)mins.Item1) + 6, DateTime.Now.Minute);
-                        mins = tm;
-                        counter++;
-                    }
-                }
-                else if (tmp == SecondsArrow)
-                    secs = Tuple.Create((secs.Item1 * 6* DateTime.Now.Second),DateTime.Now.Second);
+                if (tmp == SecondsArrow)
+                    secs = Tuple.Create((6 * DateTime.Now.Second), (6 * DateTime.Now.Second)+ 6);
             }
         }
 
